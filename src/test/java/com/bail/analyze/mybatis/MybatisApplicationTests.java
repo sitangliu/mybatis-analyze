aaae9566e624a1778133e63c3d24d055ba135b6a
sitangliu <2459615989@qq.com>
2021-12-30 01:12:47 +0800

    Initial commit

---
+++ src/test/java/com/bail/analyze/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bail.analyze.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
